cc39fdf7abaf61fd5afbef5ecd36b8e3bf1711a5
Gavin <gnswebsitedesign@gmail.com>
2014-03-21 21:36:28 -0700

    Created skeleton for DataStorage

---
+++ TideGrab/src/com/example/tidegrab/DataStorage.java
@@ -0,0 +1,14 @@
+package com.example.tidegrab;
+
+//Handles the storage of all data that needs to persist beyond the Application lifetime
+public class DataStorage {
+	private String dataSets = "res/raw/testfile.txt";
+	
+	public void writeDataSet(){
+		//Must check if data already exists for that date and station
+		//If it does, remove it and add in again
+		//If it does not, add data in
+	}
+	
+	
+}
